d8f8e45a5b4d73629bbfd75c11907bde85674ac2
HarshaVardhan <athaluri.harsha@gmail.com>
2023-12-04 10:23:02 +0530

    this keyword

--- TypeScript/demo.ts
+++ TypeScript/demo.ts
@@ -4,6 +4,13 @@ username:string = 'vivek';
 
     greet(){
         console.log('Iam Greet Method');
+        console.log('Good Morning '+this.username);
+    }
+     
+    sayHello(username){
+       let section = 'ECE';
+        console.log('Good Morning '+this.username + " belongs "+ section);
+        console.log('Good Morning '+username);
     }
 
 }
@@ -12,3 +19,4 @@ let d = new Demo();
 
 console.log(d.username);
 d.greet();
+d.sayHello('Anand');
