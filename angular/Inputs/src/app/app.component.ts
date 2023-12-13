9e42ba866791836f0d233e9f238fb54e1ee61c7f
HarshaVardhan <athaluri.harsha@gmail.com>
2023-12-13 10:12:46 +0530

    @input

---
+++ angular/Inputs/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+result:any;
+
+getValue(item:any){
+this.result = item;
+}
+
+
+}
